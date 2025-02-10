70436ae088e6a710d33452b183e8444fb13846f5
programer228 <135435622+Marcusi5482@users.noreply.github.com>
2025-02-10 15:11:07 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello World!</h1>"))
 	w.Write([]byte("<h1>Gigachad</h1>"))
-	w.Write([]byte("<h1>https://drive.google.com/drive/folders/10hGK_It0F3XWoYVaWJ6jUkv5MVIgcUJe?usp=sharing</h1>"))
+	w.Write([]byte("<h1>https://github.com/Marcusi5482/site-golang/releases/tag/1</h1>"))
 }
 
 func main() {
