c2df45d8a57e0dd8e14da60f6771685cb4b8dbab
programer228 <135435622+Marcusi5482@users.noreply.github.com>
2025-02-10 14:59:02 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello World!</h1>"))
 	w.Write([]byte("<h1>Gigachad</h1>"))
+	w.Write([]byte("<h1>https://drive.google.com/file/d/1bxkjSE9E8dVNss9IO8fE7uKvCJBm5_hc/view?usp=drive_link</h1>"))
 }
 
 func main() {
